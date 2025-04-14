b8f11acf6eefac9bc05b0f70dc2a7be615d1be07
lakonguina <laury.akonguina@icloud.com>
2025-04-14 19:02:27 +0200

    finally showing charts

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 from sqlmodel import create_engine, Session, select
 from database.model import StrategyReturn, Strategy, StrategyTransaction
 from database.model import Strategy
+from fastapi.templating import Jinja2Templates
+import os
+from pathlib import Path
+
+# Set up the correct templates directory path
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 engine = create_engine("postgresql://dinitie:dinitie@postgres:5432/dinitie")
 
@@ -11,14 +18,26 @@ def get_session():
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 
 @app.get("/")
-def index(session: Session = Depends(get_session)):
-    statement = (
+def index(request: Request, session: Session = Depends(get_session)):
+    strategy_returns = session.exec(
         select(StrategyReturn)
         .join(Strategy)
         .where(Strategy.name == "Insider Purchases")
+    ).all()
+    cac40_returns = session.exec(
+        select(StrategyReturn)
+        .join(Strategy)
+        .where(Strategy.name == "CAC 40")
+    ).all()
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "insider_purchases": strategy_returns,
+            "cac40": cac40_returns
+        }
     )
-    results = session.exec(statement).all()
-    return results
