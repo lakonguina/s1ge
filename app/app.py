d2c951edcb390b0faf99cd263730a0751646a301
lakonguina <laury.akonguina@icloud.com>
2025-04-14 18:54:30 +0200

    returns

--- app/app.py
+++ app/app.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlmodel import create_engine, Session, select
+from database.model import StrategyReturn, Strategy, StrategyTransaction
+from database.model import Strategy
+
+engine = create_engine("postgresql://dinitie:dinitie@postgres:5432/dinitie")
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 app = FastAPI()
 
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def index(session: Session = Depends(get_session)):
+    statement = (
+        select(StrategyReturn)
+        .join(Strategy)
+        .where(Strategy.name == "Insider Purchases")
+    )
+    results = session.exec(statement).all()
+    return results
